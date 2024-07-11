f94908f3833762ac11a6a4e293cc93c4077cf49a
alejandro barrantes <test.hkkd@gmail.com>
2024-07-11 04:03:12 +0000

    Sync - Testing new metric for appt.

--- models/fct_dim/dimension/dim_appointment.sql
+++
@@ -1,8 +0,0 @@
-with appt as (
-    select 
-        to_number(to_char(appointment_date, 'YYYYMMDD')) as appointment_id,
-        appointment_date, appointment_type,
-        doctor_id, patient_id 
-    from {{ ref('raw_semantic_patient') }}
-)
-select * from appt
--- models/fct_dim/dimension/dim_patient.sql
+++ models/fct_dim/dimension/dim_patient.sql
@@ -2,6 +2,6 @@ with patient as (
     select 
         patient_id, churn, start_date, churn_date,
         insurance_type,age, gender, address, medical_history
-    from {{ ref('raw_semantic_patient') }}
+    from {{ ref('stg_patient') }}
 )
 select * from patient
