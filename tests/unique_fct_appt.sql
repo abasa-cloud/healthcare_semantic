cc62ebf725adad214c869b37d90bf822ff1430dc
Alejandro Barrantes_HKKD <alejandro_barrantes@hakkoda.io>
2024-07-18 16:32:27 +0000

    sync

--- tests/unique_fct_appt.sql
+++
@@ -1,8 +0,0 @@
--- tests/unique_fact_appointments.sql
-select
-    patient_id,
-    appointment_date,
-    count(*)
-from {{ ref('fct_appointment') }}
-group by patient_id, appointment_date
-having count(*) > 1
