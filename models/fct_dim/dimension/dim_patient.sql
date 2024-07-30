5e116fbfb50a0e43e1b3249c08f717e516687415
abasa-hkkd <alejandro_barrantes@hakkoda.com>
2024-07-29 18:34:27 -0600

    Sync

--- models/fct_dim/dimension/dim_patient.sql
+++ models/fct_dim/dimension/dim_patient.sql
@@ -4,14 +4,14 @@ with patient as (
         p.churn, 
         p.start_date, 
         p.churn_date,
-        d.dim_termination_date_key,
+        d.dim_date_key,
         p.insurance_type,
         p.age, 
         p.gender, 
         p.address, 
         p.medical_history
     from {{ ref('stg_patient') }} p 
-    left join {{ ref('dim_churn_date') }} d
+    left join {{ ref('dim_corp_date') }} d
         on p.patient_id = d.patient_id 
 )
 select * from patient
